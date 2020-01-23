84b05b73543471db731eedecf6aaa543ab517744
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-01-22 20:17:15 -0600

    eating_cookies with cache

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -12,7 +12,15 @@ def eating_cookies(n, cache=None):
         return 0
     if n == 0:
         return 1
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    # return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if cache is None:
+            cache = {i: 0 for i in range(n+1)}
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + \
+            eating_cookies(n-3, cache)
+        return cache[n]
 
 
 if __name__ == "__main__":
